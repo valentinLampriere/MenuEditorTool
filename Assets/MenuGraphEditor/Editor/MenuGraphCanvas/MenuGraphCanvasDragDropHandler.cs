4c97c108807483100acf42e76a494568cdf70ada
valentinLampriere <valentin.lampriere1@gmail.com>
2025-06-01 20:17:35 +0200

    Better handle Drag&Drop to allow dropping asset from Project window

--- Assets/MenuGraphEditor/Editor/MenuGraphCanvas/MenuGraphCanvasDragDropHandler.cs
+++ Assets/MenuGraphEditor/Editor/MenuGraphCanvas/MenuGraphCanvasDragDropHandler.cs
@@ -2,6 +2,7 @@ namespace MenuGraph.Editor
 {
 	using System;
 	using UnityEditor;
+	using UnityEngine;
 	using UnityEngine.UIElements;
 	using Object = UnityEngine.Object;
 
@@ -45,8 +46,28 @@ namespace MenuGraph.Editor
 		#region Drag Drop Callbacks
 		private void OnDragUpdated(DragUpdatedEvent dragUpdatedEvent)
 		{
-			DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-			dragUpdatedEvent.StopPropagation();
+			bool isDraggingMenuUI = false;
+			Object[] draggedObjects = DragAndDrop.objectReferences;
+			foreach (Object draggedObject in draggedObjects)
+			{
+				if (draggedObject is MenuUI menuUI)
+				{
+					isDraggingMenuUI = true;
+					break;
+				}
+
+				if (draggedObject is GameObject draggedGameObject && draggedGameObject.GetComponent<MenuUI>() != null)
+				{
+					isDraggingMenuUI = true;
+					break;
+				}
+			}
+
+			if (isDraggingMenuUI == true)
+			{
+				DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+				dragUpdatedEvent.StopPropagation();
+			}
 		}
 
 		private void OnDragPerformed(DragPerformEvent dragPerformEvent)
@@ -54,7 +75,8 @@ namespace MenuGraph.Editor
 			Object[] droppedObjects = DragAndDrop.objectReferences;
 			foreach (Object droppedObject in droppedObjects)
 			{
-				if (droppedObject is MenuUI menuUI)
+				if (droppedObject is MenuUI menuUI ||
+					droppedObject is GameObject droppedGameObject && droppedGameObject.TryGetComponent(out menuUI) == true)
 				{
 					_onMenuNodeDropped?.Invoke(menuUI, dragPerformEvent);
 				}
